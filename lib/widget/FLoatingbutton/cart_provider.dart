396a40be63f1019e2d537483adc4d75fb275b8ce
Roshan09876 <khadkaroshan715@gmail.com>
2023-01-12 14:13:30 +0545

    cart_provider

--- lib/widget/FLoatingbutton/cart_provider.dart
+++ lib/widget/FLoatingbutton/cart_provider.dart
@@ -1,72 +1,72 @@
-// // ignore_for_file: unused_field, unused_element
+// ignore_for_file: unused_field, unused_element
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter/cupertino.dart';
-// import 'package:online_mobile/widget/FLoatingbutton/cart_model.dart';
-// import 'package:online_mobile/widget/FLoatingbutton/db_helper.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:online_mobile/widget/FLoatingbutton/cart_model.dart';
+import 'package:online_mobile/widget/FLoatingbutton/db_helper.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-// class CartProvider with ChangeNotifier {
-//   DBHelper db = DBHelper();
-//   int _counter = 0;
-//   int get counter => _counter;
+class CartProvider with ChangeNotifier {
+  DBHelper db = DBHelper();
+  int _counter = 0;
+  int get counter => _counter;
 
-//   double _totalPrice = 0.0;
-//   double get totalPrice => _totalPrice = 0;
+  double _totalPrice = 0.0;
+  double get totalPrice => _totalPrice = 0;
 
-//   late Future<List<Cart>> _cart;
-//   Future<List<Cart>> get cart => _cart;
+  late Future<List<Cart>> _cart;
+  Future<List<Cart>> get cart => _cart;
 
-//   Future<List<Cart>> getData() async {
-//     _cart = db.getCartList();
-//     return _cart;
-//   }
+  Future<List<Cart>> getData() async {
+    _cart = db.getCartList();
+    return _cart;
+  }
 
-//   void _setPrefItems() async {
-//     SharedPreferences prefs = await SharedPreferences.getInstance();
-//     prefs.setInt('cart_items', _counter);
-//     prefs.setDouble('total_price', _totalPrice);
-//     notifyListeners();
-//   }
+  void _setPrefItems() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setInt('cart_items', _counter);
+    prefs.setDouble('total_price', _totalPrice);
+    notifyListeners();
+  }
 
-//   void _getPrefItems() async {
-//     SharedPreferences prefs = await SharedPreferences.getInstance();
-//     _counter = prefs.getInt('cart_items') ?? 0;
-//     _totalPrice = prefs.getDouble('total_price') ?? 0.0;
-//     notifyListeners();
-//   }
+  void _getPrefItems() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _counter = prefs.getInt('cart_items') ?? 0;
+    _totalPrice = prefs.getDouble('total_price') ?? 0.0;
+    notifyListeners();
+  }
 
-//   void addTotalPrice(double productPrice) {
-//     _totalPrice = _totalPrice.abs() + productPrice.abs();
-//     _setPrefItems();
-//     notifyListeners();
-//   }
+  void addTotalPrice(double productPrice) {
+    _totalPrice = _totalPrice.abs() + productPrice.abs();
+    _setPrefItems();
+    notifyListeners();
+  }
 
-//   void removeTotalPrice(double productPrice) {
-//     _totalPrice = _totalPrice - productPrice;
-//     _setPrefItems();
-//     notifyListeners();
-//   }
+  void removeTotalPrice(double productPrice) {
+    _totalPrice = _totalPrice - productPrice;
+    _setPrefItems();
+    notifyListeners();
+  }
 
-//   double getTotalPrice() {
-//     _getPrefItems();
-//     return _totalPrice;
-//   }
+  double getTotalPrice() {
+    _getPrefItems();
+    return _totalPrice;
+  }
 
-//   void addCounter() {
-//     _counter++;
-//     _setPrefItems();
-//     notifyListeners();
-//   }
+  void addCounter() {
+    _counter++;
+    _setPrefItems();
+    notifyListeners();
+  }
 
-//   void romoverCounter() {
-//     _counter--;
-//     _setPrefItems();
-//     notifyListeners();
-//   }
+  void romoverCounter() {
+    _counter--;
+    _setPrefItems();
+    notifyListeners();
+  }
 
-//   int getCounter() {
-//     _counter = _counter < 0 ? 0 : _counter;
-//     return _counter;
-//   }
-// }
+  int getCounter() {
+    _counter = _counter < 0 ? 0 : _counter;
+    return _counter;
+  }
+}
