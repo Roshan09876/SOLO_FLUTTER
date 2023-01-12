1f3a7d5b6d0768dfd333817cee783ad468fe8034
Roshan09876 <khadkaroshan715@gmail.com>
2023-01-12 14:09:05 +0545

    themes

--- lib/widget/only_themes.dart
+++ lib/widget/only_themes.dart
@@ -1,15 +1,15 @@
-// import 'package:flutter/material.dart';
-// import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-// class MyTheme {
-//   static ThemeData lightTheme(BuildContext context) => ThemeData(
-//       brightness: Brightness.light,
-//       fontFamily: GoogleFonts.lato().fontFamily,
-//       appBarTheme: AppBarTheme(
-//           color: Colors.white,
-//           iconTheme: IconThemeData(color: Colors.black),
-//           titleTextStyle: Theme.of(context).textTheme.headline6));
+class MyTheme {
+  static ThemeData lightTheme(BuildContext context) => ThemeData(
+      brightness: Brightness.light,
+      fontFamily: GoogleFonts.lato().fontFamily,
+      appBarTheme: AppBarTheme(
+          color: Colors.white,
+          iconTheme: IconThemeData(color: Colors.black),
+          titleTextStyle: Theme.of(context).textTheme.headline6));
 
-//   static ThemeData darkTheme(BuildContext context) =>
-//       ThemeData(brightness: Brightness.dark);
-// }
+  static ThemeData darkTheme(BuildContext context) =>
+      ThemeData(brightness: Brightness.dark);
+}
