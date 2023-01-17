f4c0e682aba632581353f8f4d121e594af539ac1
Roshan09876 <khadkaroshan715@gmail.com>
2023-01-17 19:44:56 +0545

    page

--- lib/pages/Phone_sign.dart
+++ lib/pages/Phone_sign.dart
@@ -10,6 +10,17 @@ class Phone extends StatefulWidget {
 class _PhoneState extends State<Phone> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+          height: 1000,
+          width: 2000,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 72, 192, 194),
+              Color.fromARGB(172, 68, 117, 119)
+            ], begin: Alignment.topRight, end: Alignment.bottomLeft),
+          ),
+          child: Column(children: [])),
+    );
   }
 }
